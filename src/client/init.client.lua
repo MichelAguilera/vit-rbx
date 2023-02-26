1161a1338ae9ea37f0d8d4b3d0d5158eeeb8182b
MichelAguilera <48520019+MichelAguilera@users.noreply.github.com>
2023-02-26 14:12:02 -0500

    vit_rbx

--- src/client/init.client.lua
+++
@@ -1,5 +0,0 @@
-print("Hello world, from client!")
-
-local Knit = require(game:GetService("ReplicatedStorage").Common.Packages.Knit)
-
-Knit.Start():catch(warn):await()
--- src/server/init.server.lua
+++
@@ -1,5 +0,0 @@
-print("Hello world, from server!")
-
-local Knit = require(game:GetService("ReplicatedStorage").Common.Packages.Knit)
-
-Knit.Start():catch(warn):await()
